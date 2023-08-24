0a95f2f437d16d3a21afc82c3315bf6f66a4d0c8
Kaung Myat Han <arkarhtethan@gmail.com>
2023-08-24 14:05:35 +0630

    feat(): auth module finished

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+
+@Injectable()
+export class AppService {
+  constructor(@Inject(CACHE_MANAGER) private cacheService: Cache) {}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
